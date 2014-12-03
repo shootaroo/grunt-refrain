01316f9335ec958d12ab8fa851c84210c4aaf5fd
shootaroo <shotaro.tsubouchi@gmail.com>
2014-12-03 23:02:24 +0900

    Fix async

--- tasks/refrain.js
+++ tasks/refrain.js
@@ -1,15 +1,18 @@
 'use strict';
 var path = require('path');
+var async = require('async');
 
 module.exports = function (grunt) {
   grunt.registerMultiTask('refrain', 'Grunt with refrain.', function () {
+    var done = this.async();
     var refrain = require('refrain')(this.options());
-    this.files.forEach(function (filePair) {
-      filePair.src.forEach(function (src) {
+    async.eachSeries(this.files, function (filePair, next) {
+      async.eachSeries(filePair.src, function (src, next) {
         refrain.render(path.relative(refrain.options.srcDir, src), null, function (err, output) {
           grunt.file.write(filePair.dest, output);
+          next();
         });
-      });
-    });
+      }, next);
+    }, done);
   });
 };
