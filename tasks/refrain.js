e2065cc4256a8182a956d1a7d0fb88fc03b45e94
shootaroo <shotaro.tsubouchi@gmail.com>
2015-02-26 01:31:32 +0900

    Set cacheMode

--- tasks/refrain.js
+++ tasks/refrain.js
@@ -5,7 +5,10 @@ var async = require('async');
 module.exports = function (grunt) {
   grunt.registerMultiTask('refrain', 'Grunt with refrain.', function () {
     var done = this.async();
+
     var refrain = require('refrain')(this.options());
+    refrain.cacheMode = refrain.cacheMode = true;
+
     async.eachSeries(this.files, function (filePair, next) {
       async.eachSeries(filePair.src, function (src, next) {
         refrain.render(path.relative(refrain.options.srcDir, src), null, function (err, output) {
